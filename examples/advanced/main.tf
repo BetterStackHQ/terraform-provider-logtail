85e3c3f67788bb5aca53df25e4368c84000bb4a6
Petr Heinz <petr@betterstack.com>
2025-07-08 12:09:16 +0200

    remove leading whitespace

--- examples/advanced/main.tf
+++ examples/advanced/main.tf
@@ -14,13 +14,13 @@ resource "logtail_source" "this" {
   logs_retention     = 60
   metrics_retention  = 90
   vrl_transformation = <<EOT
-    # Expected msg format: [svc:router] GET /api/health succeeded in 12.345ms
-    parsed, err = parse_regex(.message, r'\[svc:(?P<service>[a-zA-Z_-]+)\] .* in (?P<duration>\d+(?:\.\d+)?)ms')
-    if (err == null) {
-        .service_name = parsed.service
-        .duration_ms = to_float!(parsed.duration)
-    }
-    EOT
+# Expected msg format: [svc:router] GET /api/health succeeded in 12.345ms
+parsed, err = parse_regex(.message, r'\[svc:(?P<service>[a-zA-Z_-]+)\] .* in (?P<duration>\d+(?:\.\d+)?)ms')
+if (err == null) {
+    .service_name = parsed.service
+    .duration_ms = to_float!(parsed.duration)
+}
+EOT
   source_group_id    = logtail_source_group.group.id
 }
 
