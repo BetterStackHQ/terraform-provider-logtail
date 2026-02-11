d4f1f5cec6fe9161d941939349d429309eed9cd6
Tomáš Mládek <tomas.mladek@betterstack.com>
2026-02-11 23:08:00 +0100

    fix versions in examples

--- examples/connection/versions.tf
+++ examples/connection/versions.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = ">= 0.13"
   required_providers {
     logtail = {
-      source  = "betterstackhq/logtail"
-      version = "0.7.7"
+      source  = "BetterStackHQ/logtail"
+      version = ">= 0.8.0"
     }
   }
 }
